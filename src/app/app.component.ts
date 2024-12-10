843766e098d693d854d2fb3ecba10d5f618def83
Vincent Horecký <vincent.horecky@seznam.cz>
2024-12-10 20:13:02 +0100

    added the two selects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import * as mainJSON from './main.json';
 
 @Component({
   selector: 'app-root',
+  imports: [ReactiveFormsModule, TitleCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  formBuilder = inject(FormBuilder);
   title = 'LaNCoDe';
+  concepts = mainJSON.concepts;
+  selectedLanguage!: string;
+  mainForm = this.formBuilder.group({
+    language: ['' as Languages],
+    concept: [''],
+  });
+
+  handleLanguageChange() {
+    this.mainForm.patchValue({ concept: '' });
+  }
 }
+
+type Languages = 'js' | 'html';
