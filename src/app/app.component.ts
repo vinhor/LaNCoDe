4be6d0b9faa9ce71246452d5066cfa2f6b57cef8
Vincent Horecký <vincent.horecky@seznam.cz>
2024-12-11 19:07:25 +0100

    made the base f the app with js's variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import * as mainJSON from './main.json';
@@ -13,15 +13,29 @@ export class AppComponent {
   formBuilder = inject(FormBuilder);
   title = 'LaNCoDe';
   concepts = mainJSON.concepts;
+  conventions: Conventions = {
+    js: { ...mainJSON.js },
+    html: { ...mainJSON.html },
+  };
   selectedLanguage!: string;
   mainForm = this.formBuilder.group({
     language: ['' as Languages],
-    concept: [''],
+    concept: ['' as Concept],
   });
 
   handleLanguageChange() {
     this.mainForm.patchValue({ concept: '' });
   }
+
+  ngOnInit() {
+    console.log(this.conventions);
+  }
 }
 
 type Languages = 'js' | 'html';
+type Concept = 'variables' | '';
+interface Conventions {
+  [language: string]: {
+    [concept: string]: string;
+  };
+}
